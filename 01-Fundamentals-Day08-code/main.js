e1aa02af3acab9e0f86a65142fa97301aac79ee3
Joel Peltonen <joel.peltonen@digitalcareerinstitute.org>
2024-07-01 13:57:42 +0200

    Add notes

--- 01-Fundamentals-Day08-code/main.js
+++ 01-Fundamentals-Day08-code/main.js
@@ -5,14 +5,23 @@ let inputElement = document.querySelector("input")
 console.log(inputElement)
 console.log(inputElement.value)
 
+let image = document.querySelector("img")
 let counter = 0
 
 function handleClick() {
     console.log("DUCK CLICK! BOOM!")
     counter = counter + 1
     console.log(counter)
+
+    // Controlling elements from JS
+    inputElement.value = counter
+    image.style.rotate = counter + "deg"
 }
 
-let image = document.querySelector("img")
-
 image.addEventListener("click", handleClick)
+image.addEventListener("mouseenter", handleClick)
+image.addEventListener("mouseout", handleClick)
+
+// 5 minute mini exercise
+// - Try "mouseenter" and "mouseout"
+// - Attach the same "handleClick" function to mouseenter and mouseout :)
